a43d8133f07abd386d4965c008b6ddb331f31aa5
jieun <dieunn@naver.com>
2022-09-11 21:39:00 +0900

    2.3 스프링이 동작하면서 생기는 일(XML을 이용하는 경우)

--- ex00/src/main/java/org/zerock/sample/Restaurant.java
+++ ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Component;
 import lombok.Data;
 import lombok.Setter;
 
-@Component
+@Component	//스프링에게 해당 클래스가 스프링에서 관리해야 하는 대상임을 표시
 @Data
 public class Restaurant {
 
-	@Setter(onMethod_ = @Autowired)
+	@Setter(onMethod_ = @Autowired)	//컴파일 시 자동으로 setChef()를 생성 + onMethod 속성: 생성되면 setChef()에 Autowired 어노테이션 추가
 	private Chef chef;
+	//Restaurant 객체는 Chef 타입의 객체를 필요로 한다는 의미
 }
