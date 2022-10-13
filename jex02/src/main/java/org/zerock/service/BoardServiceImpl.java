476a74791a227501ec050b1bb936d6eeb8004308
jieun <dieunn@naver.com>
2022-10-14 00:00:40 +0900

    9.2 비즈니스 계층의 구현과 테스트

--- jex02/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ jex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -21,38 +21,32 @@ public class BoardServiceImpl implements BoardService {
 	
 	@Override
 	public void register(BoardVO board) {
-		log.info("register........." + board);
-		mapper.insertSelectKey(board);
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public BoardVO get(Long bno) {
-		log.info("get..........." + bno);
-		
-		return mapper.read(bno);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public boolean modify(BoardVO board) {
-
-		log.info("modify.........." + board);
-		
-		return mapper.update(board) == 1;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public boolean remove(Long bno) {
-		
-		log.info("remove......" + bno);
-		
-		return mapper.delete(bno) == 1;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public List<BoardVO> getList() {
-		
-		log.info("getList.......");
-		return mapper.getList();
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 
